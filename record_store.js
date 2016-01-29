049cefab09a0a54f1ccc40fdceb2b107b47bbf5a
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-29 16:20:42 +0000

    created record store

--- record_store.js
+++ record_store.js
@@ -4,4 +4,11 @@ var Record = function(artist, title, price) {
   this.price = price;
 }
 
+var RecordStore = function(name, city, inventory) {
+  this.name = name;
+  this.city = city;
+  this.inventory = inventory;
+}
+
 module.exports.Record = Record;
+module.exports.RecordStore = RecordStore;
--- record_store_spec.js
+++ record_store_spec.js
@@ -1,7 +1,6 @@
 
 
-// Create a RecordStore that has a name, city and multiple records in it's inventory
-// Give the RecordStore a balance i.e. cash in bank.
+
 // Add some records to your RecordStore.
 // Create a method that lists the inventory.
 // Create a method so that the RecordStore can sell a record. Adjust the cash in bank to take into account the price of the record sold
@@ -10,10 +9,11 @@
 // Use TDD all the way through!
 
 var Record = require("./record_store.js").Record;
+var RecordStore = require("./record_store.js").RecordStore;
 
 var assert =  require("assert");
 
-describe('Record', function(){
+describe("Record", function(){
   // Create a constructor to create Record objects with artist, title, price
   it("should have artist, title and price", function(){
     var record60s = new Record("The Doors", "Waiting For The Sun", 12);
@@ -32,5 +32,20 @@ describe('Record', function(){
     assert.equal(record80s.price, 8);
     assert.equal(record90s.title, "Odelay");
   })
+})
+
+// Create a RecordStore that has a name, city and multiple records in it's inventory
+describe("RecordStore", function() {
+  it("should have a name, city and multiple records in its inventory", function(){
+    var record60s = new Record("The Doors", "Waiting For The Sun", 12);
+    var record70s = new Record("David Bowie", "The Rise and Fall of Ziggy Stardust", 10);
+    var record80s = new Record("Pixies", "Surfer Rosa", 8);
+    var record90s = new Record("Beck", "Odelay", 9);
+    var inventory = [record60s, record70s, record80s, record90s];
+    var myRecordStore = new RecordStore("Scratched Records", "Edinburgh", inventory);
+    assert.deepEqual(myRecordStore.inventory, [record60s, record70s, record80s, record90s]);
+  });
+
+  // Give the RecordStore a balance i.e. cash in bank.
 })
 
