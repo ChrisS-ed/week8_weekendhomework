966eee7ee874c60827e6714e3d7b43cd1e36e104
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-29 14:23:43 +0000

    created multiple records

--- record_store_spec.js
+++ record_store_spec.js
@@ -1,5 +1,5 @@
 
-// Create a few records
+
 // Create a RecordStore that has a name, city and multiple records in it's inventory
 // Give the RecordStore a balance i.e. cash in bank.
 // Add some records to your RecordStore.
@@ -20,6 +20,17 @@ describe('Record', function(){
     assert.equal(record60s.artist, "The Doors");
     assert.equal(record60s.title, "Waiting For The Sun");
     assert.equal(record60s.price, 12);
+  });
+
+  // Create a few records
+  it("should have multiple records", function(){
+    var record60s = new Record("The Doors", "Waiting For The Sun", 12);
+    var record70s = new Record("David Bowie", "The Rise and Fall of Ziggy Stardust", 10);
+    var record80s = new Record("Pixies", "Surfer Rosa", 8);
+    var record90s = new Record("Beck", "Odelay", 9);
+    assert.equal(record70s.artist, "David Bowie");
+    assert.equal(record80s.price, 8);
+    assert.equal(record90s.title, "Odelay");
   })
 })
 
