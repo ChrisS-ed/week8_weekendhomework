563899d79544412fade04b8fd38e831c660dff5c
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-29 16:44:23 +0000

    can add records to record store

--- record_store_spec.js
+++ record_store_spec.js
@@ -1,7 +1,7 @@
 
 
 
-// Add some records to your RecordStore.
+
 // Create a method that lists the inventory.
 // Create a method so that the RecordStore can sell a record. Adjust the cash in bank to take into account the price of the record sold
 // Create a method that reports on the financial situation of the store. Cash and value of inventory.
@@ -51,6 +51,21 @@ describe("RecordStore", function() {
     var myRecordStore = new RecordStore("Scratched Records", "Edinburgh");
     myRecordStore.balance = 2000;
     assert.equal(myRecordStore.balance, 2000);
+  });
+
+  // Add some records to your RecordStore.
+  it("should be able to have records added to it", function(){
+    var myRecordStore = new RecordStore("Scratched Records", "Edinburgh");
+    var record60s = new Record("The Doors", "Waiting For The Sun", 12);
+    var record70s = new Record("David Bowie", "The Rise and Fall of Ziggy Stardust", 10);
+    var record80s = new Record("Pixies", "Surfer Rosa", 8);
+    var record90s = new Record("Beck", "Odelay", 9);
+    myRecordStore.inventory = [record60s, record70s, record80s, record90s];
+    var record00s = new Record("Sufjan Stevens", "Illinois", 8);
+    var record10s = new Record("Disclosure", "Settle", 8);
+    myRecordStore.inventory.push(record00s);
+    myRecordStore.inventory.push(record10s);
+    assert.deepEqual(myRecordStore.inventory, [record60s, record70s, record80s, record90s, record00s, record10s]);
   })
 })
 
